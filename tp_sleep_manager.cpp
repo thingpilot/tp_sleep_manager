25a72c71d75d99594c4b3debc23f659caa9de599
Adam <adam@think-engineer.com>
2019-11-20 17:28:17 +0000

    Initial commit of cpp and header files

---
+++ tp_sleep_manager.cpp
@@ -0,0 +1,11 @@
+/**
+  * @file    tp_sleep_manager.cpp
+  * @version 0.1.0
+  * @author  Adam Mitchell
+  * @brief   C++ file of the Thingpilot sleep manager. This will determine when a Thingpilot
+  *          device can go to sleep and handle all of the low-level configuration to achieve this
+  */
+
+/** Includes 
+ */
+#include "tp_sleep_manager.h"
---
+++ tp_sleep_manager.h
@@ -0,0 +1,15 @@
+/**
+  * @file    tp_sleep_manager.h
+  * @version 0.1.0
+  * @author  Adam Mitchell
+  * @brief   Header file of the Thingpilot sleep manager. This will determine when a Thingpilot
+  *          device can go to sleep and handle all of the low-level configuration to achieve this
+  */
+
+/** Define to prevent recursive inclusion
+ */
+#pragma once
+
+/** Includes 
+ */
+#include <mbed.h>
